41f67fd0ac0ba42b4a4628c031a8f18aff34d399
Kamm Freudenstein <31078708+Kammuel@users.noreply.github.com>
2017-09-07 12:23:58 -0700

    Add files via upload

---
+++ dnd.js
@@ -0,0 +1,15 @@
+alert("test");
+/* function $(element)
+{
+    var elem = document.getElementById(element);
+    return elem;
+}
+ */
+//var race = $("race").selectedIndex;
+
+function getRace
+{
+    alert("test");
+    $("")
+}
+    
