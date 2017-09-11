5729210326815c69bae621e7145d61936118a540
Kamm Freudenstein <31078708+Kammuel@users.noreply.github.com>
2017-09-10 22:41:52 -0700

    Add files via upload

--- dnd.js
+++ dnd.js
@@ -1,15 +1,41 @@
-alert("test");
-/* function $(element)
+function $(element)
 {
-    var elem = document.getElementById(element);
-    return elem;
+    var element = document.getElementById("element");
+    return element
 }
- */
-//var race = $("race").selectedIndex;
 
-function getRace
+
+function getRace()
 {
-    alert("test");
-    $("")
-}
+    var race = document.getElementById("race").value;
+    alert(race);
+    var index = document.getElementById("race").selectedIndex;
+    alert(index);
     
+    /*var strength = 2;
+    $("strength").value = 2;
+    
+    -----THIS IS WHERE I AM TRYING TO RESET THE VALUE OF WHICHEVER ATTRIBUTE-----
+
+    */
+
+}
+
+function getClass()
+{
+    var classType = document.getElementById("class").value;
+    alert(classType);
+}
+
+//This function was originally designed to decrease the amount of points
+//left available to add to your character.  I was trying to do an if/else 
+//to see if the new value was higher to then decrease the points.
+function points(id)
+{
+    var oldPoints = id.value;
+    alert(oldPoints);
+    var newPoints = id.value;
+    oldPoints = newPoints;
+    alert(newPoints);
+}
+
